2133d20ab5424cb9c748c640ecedbd178b28642a
Alexandre Gomes <xandergomes@gmail.com>
2020-06-26 23:36:22 -0300

    Criando o retorno do balance Fazendo as validações de balance e criação de transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,5 @@
 import { getCustomRepository, getRepository } from 'typeorm';
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 import Category from '../models/Category';
@@ -24,6 +24,10 @@ class CreateTransactionService {
     const categoriesRepository = getRepository(Category);
 
     // Verificar se essa transação pode ser criada, validando o outcome com o total
+    const { total } = await transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Requested amount greater than the total');
+    }
 
     // Verificar se a categoria existe, caso nao exista, criar e pegar o ID
     let categoryFound = await categoriesRepository.findOne({
